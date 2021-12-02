1544ff614cdc49350d66f21471b6414ac32fac18
manduxino <73876894+manduxino@users.noreply.github.com>
2021-12-02 03:01:15 -0800

    comm

---
+++ index.js
@@ -0,0 +1,30 @@
+
+console.log("funziona");
+var a = 5;
+var b = "3";
+console.log(a+b);
+
+var port = 3000;
+var host = "localhost";
+apiServer.listen(port, host, ()=>{
+    console.log("server running at http://%s:%d");
+});
+
+apiServer.get("/", (request, response)=> {
+    console.log("get apiServer");
+    response.send("<h1>Ciao client sei in home</h1>");
+});
+
+
+var nome = "fabio";
+apiServer.get("/nome", (request,respose) =>{
+
+    console.log("richiesta get nome");
+    response.send("il mio nome è"+nome);
+});
+
+apiServer.get("/mioNome", (request,response) =>{
+
+    console.log("richiesta get mioNome",request);
+    response.send("il tuo nome è"+request    );
+});
