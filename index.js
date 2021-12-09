9f5cc4e4dc8af8676032a287e0e0aad528393897
Manduca Fabio <manduca.fabio@osdbsesto>
2021-12-09 10:47:22 +0100

    comm

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var a = 5;
 var b = "3";
 console.log(a+b);
 
+var fs= require("fs");
+
 var port = 3000;
 var host = "localhost";
 apiServer.listen(port, host, ()=>{
@@ -33,4 +35,19 @@ apiServer.get("/somma", (request,response) =>{
 
     console.log("somma request",request.query);
     response.send("risultato= "+(request.query.a -(-request.query.b)));
+});
+
+apiServer.get("/student", (request,response) =>{
+
+    console.log("student id: ",request.query.id);
+    //leggere il file
+    fs.readFile("studenti.json", (err, data) => {
+        if(err){ console.log("error: "+err)
+    }else{
+            var students = JSON.parse(data);
+            console.log("students: "+students);
+        }
+    });
+    //prelevare l'oggetto con l'id
+    //
 });
